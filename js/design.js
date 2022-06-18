28b6b6ef2abadf53062b99a27777dc0a6d2d0c53
Nelson Sammy <psammy.ngei@gmail.com>
2022-06-18 19:40:22 +0300

    first commit

---
+++ js/design.js
@@ -0,0 +1,8 @@
+// var p = document.getElementById('testData').textContent;
+// console.log(p);
+// document.addEventListener('submit', (event)=>{
+//     event.preventDefault();
+//     var name = document.getElementById('name').value;
+//     alert(name);
+//     //console.log(name);
+// });
